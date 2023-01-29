6cde60a3dbcc69fa43695381e29b50de0f438456
Rasyid Kusnady <rasyidkusnady@gmail.com>
2023-01-29 12:32:38 +0700

    feat: AboutComponent

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Info = () => {
+    return(
+        <div className="about__info grid">
+            <div className="about__box">
+                <i className="bx bx-award"></i>
+                <h3 className="about__title">Exprience</h3>
+                <span className="about__subtitle">2 Years Working</span>
+            </div>
+
+            <div className="about__box">
+                <i className="bx bx-briefcase-alt"></i>
+                <h3 className="about__title">Completed</h3>
+                <span className="about__subtitle">8 Projects</span>
+            </div>
+
+            <div className="about__box">
+                <i className="bx bx-support"></i>
+                <h3 className="about__title">Support</h3>
+                <span className="about__subtitle">Online 24/7</span>
+            </div>
+        </div>
+    )
+}
+
+export default Info
