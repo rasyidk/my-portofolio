689d8b49c68cc7fa07d52f7ca5d92f8ff0cdec71
Rasyid Kusnady <rasyidkusnady@gmail.com>
2023-02-02 21:00:36 +0700

    feat:TestimonialComponent

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,25 @@
+import Image1 from "../../assets/rasyid.jpg";
+import Image2 from "../../assets/rasyid.jpg";
+import Image3 from "../../assets/rasyid.jpg";
+
+
+export const Data = [
+    {
+        id:1,
+        image: Image1,
+        title: "john doe",
+        description : "Really to work with him, he has many skill in example eally to work with him, he has many skill in example"
+    },
+    {
+        id:2,
+        image: Image2,
+        title: "Victor <",
+        description : "GOod good good GOod good good GOod good good GOod good good"
+    },
+    {
+        id:3,
+        image: Image3,
+        title: "John V",
+        description : "mantap mantap mantap mantap mantap mantap"
+    }
+]
